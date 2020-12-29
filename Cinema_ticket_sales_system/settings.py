03184d9a6fcc48b7f6c60d04be8c5527776810c8
Alireza Nematolahy <50452187+swmnnmt@users.noreply.github.com>
2020-12-30 02:26:31 +0330

    start ticketing app

--- Cinema_ticket_sales_system/settings.py
+++ Cinema_ticket_sales_system/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ticketing.apps.TicketingConfig'
 ]
 
 MIDDLEWARE = [
@@ -104,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa-ir'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
