24c51b6f4fb4c15566bdb17d8e7354662c6149fe
Alireza Nematolahy <swmn_nmt@icloud.com>
2021-01-30 19:07:55 +0330

    Some methods added to ShowTime model

--- ticketing/models.py
+++ ticketing/models.py
@@ -5,9 +5,11 @@ class Movie(models.Model):
     """
     Represents a movie
     """
+
     class Meta:
         verbose_name = 'فیلم'
         verbose_name_plural = 'فیلم'
+
     name = models.CharField(max_length=100, verbose_name='عنوان فیلم')
     director = models.CharField(max_length=50, verbose_name='کارگردان')
     year = models.IntegerField(verbose_name='سال تولید')
@@ -23,12 +25,14 @@ class Cinema(models.Model):
     """
     Represents a cinema (movie theater)
     """
+
     class Meta:
         verbose_name = 'سینما'
         verbose_name_plural = 'سینما'
-    cinema_code = models.IntegerField(primary_key=True,verbose_name='کد سینما')
-    name = models.CharField(max_length=50,verbose_name='نام')
-    city = models.CharField(max_length=30, default='شیراز',verbose_name='شهر')
+
+    cinema_code = models.IntegerField(primary_key=True, verbose_name='کد سینما')
+    name = models.CharField(max_length=50, verbose_name='نام')
+    city = models.CharField(max_length=30, default='شیراز', verbose_name='شهر')
     capacity = models.IntegerField(verbose_name='گنجایش')
     phone = models.CharField(max_length=11, blank=True, verbose_name='تلفن')
     address = models.TextField(verbose_name='آدرس')
@@ -42,11 +46,13 @@ class ShowTime(models.Model):
     """
     Represents a movie show in a cinema at a specific time
     """
+
     class Meta:
         verbose_name = 'سانس'
         verbose_name_plural = 'سانس'
-    movie = models.ForeignKey(to=Movie, on_delete=models.PROTECT,verbose_name='فیلم')
-    cinema = models.ForeignKey(to=Cinema, on_delete=models.PROTECT,verbose_name='سینما')
+
+    movie = models.ForeignKey(to=Movie, on_delete=models.PROTECT, verbose_name='فیلم')
+    cinema = models.ForeignKey(to=Cinema, on_delete=models.PROTECT, verbose_name='سینما')
     start_time = models.DateTimeField(verbose_name='زمان شروع نمایش')
     price = models.IntegerField(verbose_name='قیمت')
     saleable_seats = models.IntegerField(verbose_name='صندلی های قابل فروش')
@@ -65,7 +71,49 @@ class ShowTime(models.Model):
         (MOVIE_PLAYED, 'فیلم پخش شد'),
         (SHOW_CANCELED, 'سانس لغو شد'),
     )
-    status = models.IntegerField(choices=status_choices,verbose_name='وضعیت')
+    status = models.IntegerField(choices=status_choices, verbose_name='وضعیت')
 
     def __str__(self):
         return '{} - {} - {}'.format(self.movie, self.cinema, self.start_time)
+
+    def get_price_display(self):
+        return '{} تومان'.format(self.price)
+
+    def is_full(self):
+        """
+        Returns True if all seats are sold
+        """
+        return self.free_seats == 0
+
+    def open_sale(self):
+        """
+        Opens ticket sale
+        If sale was opened before, raises an exception
+        """
+        if self.status == ShowTime.SALE_NOT_STARTED:
+            self.status = ShowTime.SALE_OPEN
+            self.save()
+        else:
+            raise Exception('Sale has been started before')
+
+    def close_sale(self):
+        """
+        Closes ticket sale
+        If sale is not open, raises an exception
+        """
+        if self.status == ShowTime.SALE_OPEN:
+            self.status = ShowTime.SALE_CLOSED
+            self.save()
+        else:
+            raise Exception('Sale is not open')
+
+    def expire_showtime(self, is_canceled=False):
+        """
+        Expires showtime and updates the status
+        :param is_canceled: A boolean indicating whether the show is canceled or not, default is False
+        """
+        if self.status not in (ShowTime.MOVIE_PLAYED, ShowTime.SHOW_CANCELED):
+            self.status = ShowTime.SHOW_CANCELED if is_canceled else ShowTime.MOVIE_PLAYED
+            self.save()
+        else:
+            raise Exception('Show has been expired before')
