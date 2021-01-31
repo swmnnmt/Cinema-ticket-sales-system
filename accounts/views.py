f8a9e36bd566be86dfc0692bdae17417a6319723
Alireza Nematolahy <swmn_nmt@icloud.com>
2021-01-31 17:21:22 +0330

    #12 profile edit forms and page added

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from accounts.forms import PaymentForm, ProfileForm, MyUserForm
+from accounts.models import Payment
 
 
 def login_view(request):
@@ -39,3 +41,49 @@ def profile_details(request):
         'profile': profile
     }
     return render(request, 'accounts/profile_details.html', context)
+
+
+@login_required
+def payment_list(request):
+    payments = Payment.objects.filter(profile=request.user.profile).order_by('-transaction_time')
+    context = {
+        'payments': payments
+    }
+    return render(request, 'accounts/payment_list.html', context)
+
+
+@login_required
+def payment_create(request):
+    if request.method == 'POST':
+        payment_form = PaymentForm(request.POST)
+        if payment_form.is_valid():
+            payment = payment_form.save(commit=False)
+            payment.profile = request.user.profile
+            payment.save()
+            request.user.profile.deposit(payment.amount)
+            return HttpResponseRedirect(reverse('accounts:payment_list'))
+    else:
+        payment_form = PaymentForm()
+    context = {
+        'payment_form': payment_form
+    }
+    return render(request, 'accounts/payment_create.html', context)
+
+
+@login_required
+def profile_edit(request):
+    if request.method == 'POST':
+        user_form = MyUserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, files=request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return HttpResponseRedirect(reverse('accounts:profile_details'))
+    else:
+        user_form = MyUserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form
+    }
+    return render(request, 'accounts/profile_edit.html', context)
